841d9e0d5c921bcad4cfd5780d87bdfdd39d61fc
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2025-02-01 09:50:28 +0500

    add CSRF_TRUSTED_ORIGINS

--- data/config.py
+++ data/config.py
@@ -5,7 +5,8 @@ env.read_env()
 
 DJANGO_SECRET_KEY = env.str('SECRET_KEY')
 DJANGO_DEBUG = env.bool('DEBUG', default=True)
-DJANGO_ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+DJANGO_ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost','0.0.0.0','127.0.0.1'])
+DJANGO_CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 
 ADMINS = env.list('ADMINS')
 TELEGRAM_BOT_TOKEN = env.str('TELEGRAM_BOT_TOKEN')
